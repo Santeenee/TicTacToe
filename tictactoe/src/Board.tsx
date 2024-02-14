46857fa43e93b96e4a89b616a7b9cf0c86ad605a
Francesco Santini <francescotn02@gmail.com>
2024-02-14 21:50:54 +0100

    refactoring

--- tictactoe/src/Board.tsx
+++ tictactoe/src/Board.tsx
@@ -1,127 +1,129 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export default function Board() {
-	//contains the values of the cells
-	const [boardArr, setBoardArr] = useState<['' | 'X' | 'O']>([''])
-	const [times, setTimes] = useState<number>(0)
+  //contains the values of the cells
+  const [boardArr, setBoardArr] = useState<("" | "X" | "O")[]>(new Array(9));
+  const [times, setTimes] = useState<number>(0);
 
-	useMemo(() => resetBoard(), [])
+  useEffect(() => resetBoard(), []);
 
-	//every time the board updates, check the winner
-	useMemo(() => {
-		if (times > 4) {
-			let winner: any = checkWinner() //!
-			console.log(winner)
-			if (winner != undefined) {
-				printWinner(winner)
-			}
-		}
-	}, [boardArr])
+  //every time the board updates, check the winner
+  useEffect(() => {
+    if (times > 4) {
+      const winner = checkWinner();
+      console.log(winner);
+      if (winner !== undefined) {
+        printWinner(winner);
+      }
+    }
+  }, [boardArr]);
 
-	function resetBoard() {
-		setTimes(0)
-		setBoardArr((boardArr): any => ['', '', '', '', '', '', '', '', '']) //! ANY
-	}
+  function resetBoard() {
+    setTimes(0);
+    setBoardArr((boardArr) => ["", "", "", "", "", "", "", "", ""]);
+  }
 
-	function printWinner(x: 'PlayerX' | 'PlayerO' | 'Tie') {
-		//create a <div>
-		let winnerPopup = document.createElement('div')
+  function printWinner(x: "PlayerX" | "PlayerO" | "Tie") {
+    //create a <div>
+    const winnerPopup = document.createElement("div");
 
-		x === 'Tie'
-			? (winnerPopup.innerHTML = `<p>It's a ${x}!</p>`)
-			: (winnerPopup.innerHTML = `<p>The winner is ${x}!</p>`)
+    if (x === "Tie") winnerPopup.innerHTML = `<p>It's a ${x}!</p>`;
+    else winnerPopup.innerHTML = `<p>The winner is ${x}!</p>`;
 
-		winnerPopup.classList.add('winner-popup')
+    winnerPopup.classList.add("winner-popup");
 
-		//remove mouse interactions
-		const boardElem = document.querySelector('.board') as HTMLElement
-		boardElem.style.pointerEvents = 'none'
+    //remove mouse interactions
+    const boardElem = document.querySelector(".board") as HTMLElement;
+    boardElem.style.pointerEvents = "none";
 
-		//add <div> to the document
-		document.querySelector('#root')?.appendChild(winnerPopup)
+    //add <div> to the document
+    document.querySelector("#root")?.appendChild(winnerPopup);
 
-		setTimeout(() => {
-			winnerPopup.remove()
+    setTimeout(() => {
+      winnerPopup.remove();
 
-			//re-add mouse interactions
-			boardElem.style.pointerEvents = 'auto'
+      //re-add mouse interactions
+      boardElem.style.pointerEvents = "auto";
 
-			resetBoard()
-		}, 3000)
-	}
+      resetBoard();
+    }, 3000);
+  }
 
-	function checkWinner() {
-		let b = [...boardArr]
-		console.log(b)
+  function checkWinner() {
+    const board = boardArr;
+    console.log(board);
 
-		//check horizontal
-		for (let h = 2; h < 8; h += 3) {
-			if (b[h] !== '' && b[h] === b[h - 1] && b[h - 1] === b[h - 2]) {
-				return `Player${b[h]}`
-			}
-		}
+		//TODO fix bug here
+    //check horizontal
+    for (let h = 2; h < 8; h += 3) {
+      if (board[h] !== "" && board[h] === board[h - 1] && board[h - 1] === board[h - 2]) {
+        return `Player${board[h]}`;
+      }
+    }
 
-		//check vertical
-		for (let v = 0; v < 3; v++) {
-			if (b[v] !== '' && b[v] === b[v + 3] && b[v + 3] === b[v + 6]) {
-				return `Player${b[v]}`
-			}
-		}
+    //check vertical
+    for (let v = 0; v < 3; v++) {
+      if (board[v] !== "" && board[v] === board[v + 3] && board[v + 3] === board[v + 6]) {
+        return `Player${board[v]}`;
+      }
+    }
 
-		//check diagonals
-		if (b[0] !== '' && b[0] === b[4] && b[4] === b[8]) {
-			return `Player${b[0]}`
-		} else if (b[2] !== '' && b[2] === b[4] && b[4] === b[6]) {
-			return `Player${b[2]}`
-		}
+    //check diagonals
+    if (board[0] !== "" && board[0] === board[4] && board[4] === board[8]) {
+      return `Player${board[0]}`;
+    }
+    if (board[2] !== "" && board[2] === board[4] && board[4] === board[6]) {
+      return `Player${board[2]}`;
+    }
 
-		//when board is full && all values !== ''
-		//then 'Tie'
-		let bfiltered = b.filter(cell => cell !== '')
-		console.log('b.filter è:')
-		console.log(bfiltered)
-		if (bfiltered.length === 9) {
-			return 'Tie'
-		}
+    //when board is full && all values !== ''
+    //then 'Tie'
+    const bfiltered = board.filter((cell) => cell !== "");
+    console.log("b.filter è:");
+    console.log(bfiltered);
+    if (bfiltered.length === 9) {
+      return "Tie";
+    }
 
-		//no winner, no tie, the game goes on
-		return //same as 'return undefined'
-	}
+    //no winner, no tie, the game goes on
+    return;
+  }
 
-	function handleClick(index: number) {
-		if (boardArr[index] === '') {
-			let newArr: ['' | 'X' | 'O'] = [...boardArr]
+  function handleClick(index: number) {
+    if (boardArr[index] === "") {
+      const newArr: ("" | "X" | "O")[] = boardArr;
 
-			if (times % 2 === 0) {
-				newArr[index] = 'X'
-				setBoardArr(newArr)
-			}
-			if (times % 2 === 1) {
-				newArr[index] = 'O'
-				setBoardArr(newArr)
-			}
-			setTimes(times => ++times)
-		}
-	}
+      if (times % 2 === 0) {
+        newArr[index] = "X";
+        setBoardArr(newArr);
+      }
+      else {
+        newArr[index] = "O";
+        setBoardArr(newArr);
+      }
+      setTimes((times) => ++times);
+    }
+  }
 
-	return (
-		<div className="board">
-			{boardArr.map((item, index: number) => (
-				<button
-					role="button"
-					className="cell"
-					onClick={() => handleClick(index)}
-					key={index}
-				>
-					{item === 'X' ? (
-						<img src="assets/X.svg" />
-					) : item === 'O' ? (
-						<img src="assets/O.svg" />
-					) : (
-						''
-					)}
-				</button>
-			))}
-		</div>
-	)
+  return (
+    <div className="board">
+      {boardArr.map((item, index: number) => (
+        // biome-ignore lint/a11y/useButtonType: <explanation>
+        <button
+          role="button"
+          className="cell"
+          onClick={() => handleClick(index)}
+          key={index} //Biome doesn't like this element
+        >
+          {item === "X" ? (
+            <img src="assets/X.svg" alt="X symbol" />
+          ) : item === "O" ? (
+            <img src="assets/O.svg" alt="O symbol" />
+          ) : (
+            ""
+          )}
+        </button>
+      ))}
+    </div>
+  );
 }
